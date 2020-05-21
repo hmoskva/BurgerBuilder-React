ad6e739bdf462d90035889e3fe3a0be9dde4d3f8
Habib Sogbesan <h.d.sogbesan@gmail.com>
2020-05-21 12:56:44 +0100

    progress:sections1-9

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const Burger = (props) => {
+  console.log(props.ingredients, "INs");
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => (
+        <BurgerIngredient key={igKey + i} type={igKey} />
+      ));
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  //   const isEmptyIngredients = transformedIngredients.every(
+  //     (igArray) => !igArray.length
+  //   );
+  //   console.log(isEmptyIngredients, "isEmptyIngredients");
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
